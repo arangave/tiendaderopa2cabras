17247814fef0c5c6e286247d20640a4f056a9a7b
arangave <arancha.garciaavellan@gmail.com>
2025-06-12 16:48:33 +0200

    favicon añadido

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./styles/globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ThemeToggleButton from "./components/ThemeToggleButton"; // <--- IMPORTA AQUÍ
+import ThemeToggleButton from "./components/ThemeToggleButton";
 
 export const metadata = {
   title: "2CabrasConTraje",
@@ -13,11 +13,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="es">
       <body className="bg-white dark:bg-[#181818] text-black dark:text-white dark:bg-black dark:text-white transition-colors duration-300">
         <Header />
-        <ThemeToggleButton /> {/* <-- AGRÉGALO AQUÍ */}
+        <ThemeToggleButton />
         <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
   );
 }
-
